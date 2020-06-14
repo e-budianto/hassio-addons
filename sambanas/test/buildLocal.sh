22c04d141923f73305420ff0091afa1a4a5d6617
Edward <e-budianto@users.noreply.github.com>
2020-06-14 22:54:26 +0700

    Update buildLocal.sh

--- sambanas/test/buildLocal.sh
+++ sambanas/test/buildLocal.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 # onetime when docker upgrade 
 # docker run --rm --privileged multiarch/qemu-user-static:register
-docker build --build-arg BUILD_FROM="homeassistant/armv7-base:latest" -t dianlight/armv7-addon-sambanas ..
-docker push dianlight/armv7-addon-sambanas:latest
+docker build --build-arg BUILD_FROM="homeassistant/armv7-base:latest" -t e-budianto/armv7-addon-sambanas ..
+docker push e-budianto/armv7-addon-sambanas:latest
