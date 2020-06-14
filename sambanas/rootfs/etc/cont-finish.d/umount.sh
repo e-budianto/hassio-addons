3b7de599ff83e86858a3fbb29688f6ac516a9b71
Edward <e-budianto@users.noreply.github.com>
2020-06-14 10:10:10 +0700

    Update umount.sh

--- sambanas/rootfs/etc/cont-finish.d/umount.sh
+++ sambanas/rootfs/etc/cont-finish.d/umount.sh
@@ -5,3 +5,6 @@
 bashio::log.info "Unmount drivers."
 umount -a
 bashio::log.info "Done."
+bashio::log.info "Mounting external Flashdisk 64GB."
+ssh pi@hassio4 './mount64gb.sh'
+bashio::log.info "Done mounting."
